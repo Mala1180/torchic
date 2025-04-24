69e838d2e7503a362c504f84871c1636cbcf9a6e
Mattia Matteini <matteinimattia@gmail.com>
2025-04-24 15:37:09 +0200

    refactor(builder)!: update name creation of layers, remove reset layers when build nn object, add reset method

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -13,27 +13,65 @@ def builder() -> NeuralNetworkBuilder:
 
 
 class TestNeuralNetworkBuilder:
+    def test_create_linear_layers(self, builder: NeuralNetworkBuilder) -> None:
+        expected: nn.Module = nn.ModuleDict(
+            {
+                "linear1": nn.Linear(20, 30),
+                "linear2": nn.Linear(20, 10),
+                "linear3": nn.Linear(10, 1),
+            }
+        )
+        actual: NeuralNetwork = (
+            builder.add_linear(20, 30).add_linear(20, 10).add_linear(10, 1).build()
+        )
+        self.__compare_models(expected, actual.layers)
 
-    def test_simple_linear_relu(self, builder: NeuralNetworkBuilder) -> None:
+    def test_create_generic_layers(self, builder: NeuralNetworkBuilder) -> None:
         # Build using standard PyTorch
         expected: nn.Module = nn.ModuleDict(
             {
                 "linear1": nn.Linear(10, 20),
-                "relu": nn.ReLU(),
+                "relu1": nn.ReLU(),
+                "tanh1": nn.Tanh(),
                 "linear2": nn.Linear(20, 1),
             }
         )
 
         # Build using NeuralNetworkBuilder
         actual: NeuralNetwork = (
-            builder.add_layer("linear1", nn.Linear(10, 20))
-            .add_layer("relu", nn.ReLU())
-            .add_layer("linear2", nn.Linear(20, 1))
+            builder.add_layer(nn.Linear(10, 20))
+            .add_layer(nn.ReLU())
+            .add_layer(nn.Tanh())
+            .add_layer(nn.Linear(20, 1))
             .build()
         )
-        self.__compare_model(expected, actual.layers)
+        self.__compare_models(expected, actual.layers)
 
-    def __compare_model(self, expected: nn.Module, actual: nn.Module) -> None:
+    def test_create_parallel_layers(self, builder: NeuralNetworkBuilder) -> None:
+        # Build using standard PyTorch
+        expected: nn.Module = nn.ModuleDict(
+            {
+                "linear1": nn.Linear(10, 20),
+                "relu1": nn.ReLU(),
+                "parallel1": nn.ModuleDict(
+                    {
+                        "linear2": nn.Linear(20, 1),
+                        "linear3": nn.Linear(20, 1),
+                    }
+                ),
+            }
+        )
+
+        # Build using NeuralNetworkBuilder
+        actual: NeuralNetwork = (
+            builder.add_layer(nn.Linear(10, 20))
+            .add_layer(nn.ReLU())
+            .add_parallel({"linear2": nn.Linear(20, 1), "linear3": nn.Linear(20, 1)})
+            .build()
+        )
+        self.__compare_models(expected, actual.layers)
+
+    def __compare_models(self, expected: nn.Module, actual: nn.Module) -> None:
         assert repr(expected) == repr(actual)
         assert str(expected) == str(actual)
         for layer_expected, layer_actual in zip(expected.modules(), actual.modules()):
--- torchic/nn/builder/__init__.py
+++ torchic/nn/builder/__init__.py
@@ -1,5 +1,4 @@
-from copy import deepcopy
-from typing import Dict, Callable
+from typing import Dict, Callable, Optional, Set
 
 import torch
 import torch.nn as nn
@@ -22,20 +21,47 @@ class NeuralNetworkBuilder:
         self.layers: nn.ModuleDict = nn.ModuleDict()
         self.device: torch.device = device
 
-    def add_layer(self, name: str, layer: nn.Module) -> "NeuralNetworkBuilder":
+    def add_layer(
+        self, layer: nn.Module, name: Optional[str] = None
+    ) -> "NeuralNetworkBuilder":
         """
-        Add a layer to the network.
+        Add a generic layer to the network.
         :param name: layer name
         :param layer: nn.Module layer
         :return: self
         """
-        if name in self.layers:
-            raise ValueError(f"Layer name '{name}' already exists.")
+        self.__assert_layer_name(name)
+        if name is None:
+            name = self.__provide_layer_name(layer.__class__.__name__.lower())
         self.layers[name] = layer
         return self
 
+    def add_linear(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        name: Optional[str] = None,
+    ) -> "NeuralNetworkBuilder":
+        """
+        Add a linear layer to the network.
+        :param in_features: number of input features
+        :param out_features: number of output features
+        :param bias: whether to include a bias term
+        :param name: layer name
+        :return: self
+        """
+        self.__assert_layer_name(name)
+        linear_layer = nn.Linear(in_features, out_features, bias)
+        if name is None:
+            name = self.__provide_layer_name("linear")
+        self.layers[name] = linear_layer
+        return self
+
     def add_parallel(
-        self, name: str, layer_dict: Dict[str, nn.Module]
+        self,
+        layer_dict: Dict[str, nn.Module] | Set[nn.Module],
+        name: Optional[str] = None,
     ) -> "NeuralNetworkBuilder":
         """
         Add parallel layers as a single block.
@@ -43,13 +69,25 @@ class NeuralNetworkBuilder:
         :param layer_dict: dictionary of nn.Module layers
         :return: self
         """
-        if name in self.layers:
-            raise ValueError(f"Layer name '{name}' already exists.")
+        self.__assert_layer_name(name)
+        if name is None:
+            name = self.__provide_layer_name("parallel")
+        if isinstance(layer_dict, set):
+            layer_dict = {
+                self.__provide_layer_name(layer.__class__.__name__.lower()): layer
+                for layer in layer_dict
+            }
+        elif isinstance(layer_dict, dict):
+            pass
+        else:
+            raise TypeError(
+                "layer_dict must be of type Dict[str, torch.nn.Module] or Set[nn.Module]."
+            )
         self.layers[name] = nn.ModuleDict(layer_dict)
         return self
 
-    def add_transform_layer(
-        self, name: str, transform_fn: Callable[[Tensor], Tensor]
+    def add_transform(
+        self, transform_fn: Callable[[Tensor], Tensor], name: Optional[str] = None
     ) -> "NeuralNetworkBuilder":
         """
         Add a transformation layer to the network.
@@ -58,8 +96,9 @@ class NeuralNetworkBuilder:
         :param transform_fn: transformation function
         :return: self
         """
-        if name in self.layers:
-            raise ValueError(f"Layer name '{name}' already exists.")
+        self.__assert_layer_name(name)
+        if name is None:
+            name = self.__provide_layer_name("transform")
         self.layers[name] = TransformLayer(transform_fn)
         return self
 
@@ -70,6 +109,28 @@ class NeuralNetworkBuilder:
         """
         if len(self.layers) == 0:
             raise ValueError("No layers have been added to the network.")
-        layers: nn.ModuleDict = deepcopy(self.layers)
+        return NeuralNetwork(self.layers).to(self.device)
+
+    def reset(self) -> None:
+        """
+        Reset the builder to its initial state.
+        :return: None
+        """
         self.layers.clear()
-        return NeuralNetwork(layers).to(self.device)
+
+    def __assert_layer_name(self, name: Optional[str]) -> None:
+        if name is not None and name in self.layers:
+            raise ValueError(f"Layer name '{name}' already exists.")
+
+    def __provide_layer_name(self, base_name: str) -> str:
+        """
+        Provide a unique name for the layer.
+        :param base_name: base name for the layer
+        :return: unique layer name
+        """
+        counter: int = 1
+        name: str = base_name + str(counter)
+        while name in self.layers:
+            name = base_name + str(counter)
+            counter += 1
+        return name
